b08901172c04c605985f6390430a8332992e9094
pollchihuy <poll.chihuy@gmail.com>
2025-12-16 21:15:35 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/pcmspringboot29/Pcmspringboot29Application.java
@@ -0,0 +1,13 @@
+package com.juaracoding.pcmspringboot29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pcmspringboot29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pcmspringboot29Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/pcmspringboot29/Pcmspringboot29ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.pcmspringboot29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pcmspringboot29ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
